0ed971d0f713621208159796babfa5ef060c1dec
ran.zhou <ran.zhou@aptiv.com>
2019-02-28 19:06:02 +0800

    Add hexo init.sh

---
+++ source/init.sh
@@ -0,0 +1,7 @@
+sudo npm install hexo --save
+sudo npm install hexo-server --save
+sudo npm install hexo-deployer-git --save
+sudo npm install
+
+#sudo npm install hexo-fs --save
+#sudo npm audit fix
