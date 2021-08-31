a5468a643baf1a385aaab489dcb3f4c3f2243ed1
ran.zhou <ran.zhou@aptiv.com>
2021-08-31 13:44:26 +0800

    Update music list

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -3,6 +3,24 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [{
+        name: 'Five Hundred Miles',
+        artist: 'Justin Timberlake',
+        url: '/dist/MusicList/FiveHundredMiles.mp3',
+        cover: 'http://p2.music.126.net/ggnyubDdMxrhpqYvpZbhEQ==/3302932937412681.jpg?param=130y130',
+    },
+    {
+        name: "成都",
+        artist: '赵雷',
+        url: 'https://www.ytmp3.cn/down/56349.mp3',
+        cover: 'http://p1.music.126.net/c5NVKUIAUcyN4BQUDbGnEg==/109951163221157827.jpg?param=130y130',
+    },
+    {
+        name: "呓语",
+        artist: '蒋卓林',
+        url: 'https://www.ytmp3.cn/down/50305.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+    },
+    {
         name: '麻雀',
         artist: '李荣浩',
         url: '/dist/MusicList/麻雀.mp3',
@@ -12,6 +30,12 @@ const ap = new APlayer({
         name: "平凡之路",
         artist: '朴树',
         url: 'http://www.ytmp3.cn/down/59211.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+    },
+    {
+        name: "好久不见",
+        artist: '陈奕迅',
+        url: 'https://www.ytmp3.cn/down/53654.mp3',
         cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
     },
     {
@@ -27,15 +51,9 @@ const ap = new APlayer({
         cover: 'http://p1.music.126.net/LiRR__0pJHSivqBHZzbMUw==/109951163816225567.jpg?param=130y130',
     },
     {
-        name: 'Something Just Like This',
-        artist: 'The Chainsmokers',
-        url: 'http://www.ytmp3.cn/down/50463.mp3',
+        name: 'On a Slow Boat to China',
+        artist: 'Luke Thompson',
+        url: '/dist/MusicList/OnaSlowBoatToChina.mp3',
         cover: 'http://p2.music.126.net/ggnyubDdMxrhpqYvpZbhEQ==/3302932937412681.jpg?param=130y130',
-    },
-    {
-        name: 'Good Time',
-        artist: 'Owl City&Carly Rae Jepsen',
-        url: 'http://www.ytmp3.cn/down/34148.mp3',
-        cover: 'http://p1.music.126.net/c5NVKUIAUcyN4BQUDbGnEg==/109951163221157827.jpg?param=130y130',
     }]
 });
