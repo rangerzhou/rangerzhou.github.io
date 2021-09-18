27342c119901572dd565a755f1a25137a02e09b3
ran.zhou <ran.zhou@aptiv.com>
2021-09-18 15:35:43 +0800

    Upload 平凡之路.mp3

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -29,7 +29,7 @@ const ap = new APlayer({
     {
         name: "平凡之路",
         artist: '朴树',
-        url: 'http://www.ytmp3.cn/down/59211.mp3',
+        url: '/dist/MusicList/平凡之路.mp3',
         cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
     },
     {
